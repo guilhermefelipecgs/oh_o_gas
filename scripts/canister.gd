794d5e804b40316f81f99484e41d77996519386c
Guilherme Felipe <guilhermefelipecgs@gmail.com>
2016-12-23 22:54:09 -0200

    Ajuste da resolução e adicão de sprites em pixel

--- scripts/canister.gd
+++ scripts/canister.gd
@@ -1,14 +1,5 @@
 extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
 func _on_canister_body_enter( body ):
 	if body.get_name() == "floor":
 		set_mode(MODE_STATIC)
--- scripts/truck.gd
+++ scripts/truck.gd
@@ -1,13 +1,14 @@
 extends RigidBody2D
 
-var canister = preload("res://scenes/canister.tscn")
 const LEFT = Vector2(-1,-1)
 const RIGHT = Vector2(1,-1)
 const MAX_THROW = 350
-const MIN_THROW = 220
+const MIN_THROW = 200
+const MAX_TIME = 800
+
+var canister = preload("res://scenes/canister.tscn")
 var left_time_press
 var right_time_press
-const MAX_TIME = 800
 
 func _ready():
 	set_process_input(true)
@@ -23,19 +24,17 @@ func _input(event):
 	elif event.is_action_released("ui_right"):
 		throw(RIGHT, OS.get_ticks_msec() - right_time_press)
 
-
-func _process(delta):
-	pass
-
 func throw(direction, delta_time_press):
 	var spawn_canister = canister.instance()
 	var force = MAX_THROW * delta_time_press / MAX_TIME
 	force = clamp(force, MIN_THROW, MAX_THROW)
+	
+	spawn_canister.set_pos(get_pos())
 	if(direction == LEFT):
 		spawn_canister.set_angular_velocity(5)
 		spawn_canister.apply_impulse(Vector2(), LEFT * force)
 	else:
 		spawn_canister.set_angular_velocity(-5)
 		spawn_canister.apply_impulse(Vector2(), RIGHT * force)
-	spawn_canister.set_pos(get_pos())
+		
 	get_parent().add_child(spawn_canister)
