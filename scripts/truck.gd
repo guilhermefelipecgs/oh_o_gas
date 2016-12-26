1a29fa2fe8e2c842f582013a28b0ac7f24f2989b
Daniel <cgimenez.daniel@gmail.com>
2016-12-26 15:19:55 -0200

    pressbar introduced

--- scripts/truck.gd
+++ scripts/truck.gd
@@ -2,16 +2,49 @@ extends RigidBody2D
 
 const LEFT = Vector2(-1,-1)
 const RIGHT = Vector2(1,-1)
-const MAX_THROW = 350
+const MAX_THROW = 260
 const MIN_THROW = 50
-const MAX_TIME = 500
+const MAX_TIME = 1000
 
+var force = 0
 var canister = preload("res://scenes/canister.tscn")
-var left_time_press
-var right_time_press
+var left_time_press = 0
+var right_time_press = 0
+var delta_left
+var delta_right
+var max_pressbar_scale
+var left_pressbar
+var right_pressbar
 
 func _ready():
+	left_pressbar = get_parent().get_node("HUD/left_pressbar")
+	right_pressbar = get_parent().get_node("HUD/right_pressbar")
+	max_pressbar_scale = left_pressbar.get_scale()
 	set_process_input(true)
+	set_process(true)
+	
+func _process(delta):
+	delta_left = 0
+	delta_right = 0
+	if left_time_press > 0:
+		delta_left = OS.get_ticks_msec() - left_time_press
+		if(delta_left >= MAX_TIME):
+			throw(LEFT, delta_left)
+	
+	if right_time_press > 0:
+		delta_right = OS.get_ticks_msec() - right_time_press
+		if(delta_right >= MAX_TIME):
+			throw(RIGHT, delta_right)
+
+	if delta_left:
+		left_pressbar.set_scale(Vector2(min(delta_left * max_pressbar_scale.x / MAX_TIME, max_pressbar_scale.x), max_pressbar_scale.y))
+	else:
+		left_pressbar.set_scale(Vector2(0, max_pressbar_scale.y))
+		
+	if delta_right:
+		right_pressbar.set_scale(Vector2(min(delta_right * max_pressbar_scale.x / MAX_TIME, max_pressbar_scale.x), max_pressbar_scale.y))
+	else:
+		right_pressbar.set_scale(Vector2(0, max_pressbar_scale.y))
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
@@ -25,15 +58,20 @@ func _input(event):
 		throw(RIGHT, OS.get_ticks_msec() - right_time_press)
 
 func throw(direction, delta_time_press):
+
 	var spawn_canister = canister.instance()
-	var force = min(delta_time_press * (MAX_THROW - MIN_THROW) / MAX_TIME + MIN_THROW, MAX_THROW)
+	force = min(delta_time_press * (MAX_THROW - MIN_THROW) / MAX_TIME + MIN_THROW, MAX_THROW)
 	
 	spawn_canister.set_pos(get_pos())
 	if(direction == LEFT):
+		if left_time_press == 0: return
 		spawn_canister.set_angular_velocity(5)
 		spawn_canister.apply_impulse(Vector2(), LEFT * force)
+		left_time_press = 0
 	else:
+		if right_time_press == 0: return
 		spawn_canister.set_angular_velocity(-5)
 		spawn_canister.apply_impulse(Vector2(), RIGHT * force)
+		right_time_press = 0
 		
 	get_parent().get_parent().add_child(spawn_canister)
