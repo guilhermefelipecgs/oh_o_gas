dae14977da5890f2580e50d04a35df8d8dac1e0d
Daniel <cgimenez.daniel@gmail.com>
2016-12-29 19:40:57 -0200

    stuff

--- scripts/human.gd
+++ scripts/human.gd
@@ -3,8 +3,11 @@ extends RigidBody2D
 
 var go_home = false
 var scared = false
+var walk_path = Vector2()
+var pos
 
 func _ready():
+	pos = get_pos()
 	set_process(true)
 
 func _process(delta):
@@ -13,6 +16,12 @@ func _process(delta):
 			set_pos(Vector2(get_pos().x-1,get_pos().y))
 		else:
 			set_pos(Vector2(get_pos().x-4,get_pos().y))
+	else:
+		if get_pos().distance_to(walk_path):
+			var dir = (get_pos() - walk_path).normalized()
+			pos += dir * 1
+			print(pos)
+			set_pos(pos)
 	
 func go_home(scared = false):
 	self.scared = scared
@@ -21,4 +30,4 @@ func go_home(scared = false):
 
 func die():
 	global.score += 100
-	#queue_free()
+	queue_free()
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,12 +1,20 @@
 extends Node
 
-var house = preload("res://scenes/human.tscn")
+var house = preload("res://scenes/house.tscn")
+var human = preload("res://scenes/human.tscn")
+var last_spawn_time = 0.0
+
+const SPAWN_TIME = 2000.0
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	
-	var house = self.house.instance()
-	add_child(house)
+	if(OS.get_ticks_msec() - last_spawn_time >= SPAWN_TIME):
+		var house = self.house.instance()
+		var human = self.human.instance()
+		human.walk_path = house.get_node("walk_path").get_pos()
+		house.get_node("spawn_place").add_child(human)
+		add_child(house)
+		last_spawn_time = OS.get_ticks_msec()
 	
