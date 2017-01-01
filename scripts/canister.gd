d54d37bab014b16a357a4dc94d73dbdc96b0edf0
Guilherme Felipe <guilherme@localhost.localdomain>
2017-01-01 17:27:15 -0200

    Fix explosion direction

--- scripts/canister.gd
+++ scripts/canister.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
 var move
+var local_direction
 
 func _process(delta):
 	if move == null: move = get_pos()
-#	if(get_pos().x > 
-	move += global.house_direction * global.house_speed
+
+	if(get_pos().x < get_viewport_rect().size.width / 2) :
+		local_direction = global.house_direction
+	else:
+		local_direction = global.house_direction * Vector2(-1, 1)
+		
+	move += local_direction * global.house_speed
 	set_pos(move)
 
 func _on_canister_body_enter( body ):
