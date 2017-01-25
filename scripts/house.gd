868bdf0ae1d2b52b7d73cb50619fa0882f20fb9d
Guilherme Felipe <guilhermefelipecgs@gmail.com>
2017-01-25 19:18:36 -0200

    Fix code

--- scripts/house.gd
+++ scripts/house.gd
@@ -18,9 +18,9 @@ func _on_VisibilityNotifier2D_exit_screen():
 
 func destroy():
 	var dm = damage_message.instance()
-	dm.set_text("+50")
+	dm.get_node("Label").set_text("+50")
 	get_tree().get_root().add_child(dm)
-	dm.set_pos(get_node("message_position").get_global_pos() - Vector2(dm.get_size().x/2, dm.get_size().y/2))
+	dm.get_node("Label").set_pos(get_node("message_position").get_global_pos() - Vector2(dm.get_node("Label").get_size().x/2, dm.get_node("Label").get_size().y/2))
 	
 	global.score += 50
 	get_node("Sprite").set_frame(2)
